60bb1f70ac287a17a976d6222a238be783d0783e
elalerivas <52897010+elalerivas@users.noreply.github.com>
2022-12-22 02:16:44 -0300

    Update SimplyPlot.py

--- SimplyPlot.py
+++ SimplyPlot.py
@@ -379,12 +379,12 @@ with tab2:
         
 
         ff #muestra graficos
-        plt.savefig('grafico23.png')
+        plt.savefig('https://github.com/elalerivas/Post-tuloUTN/grafico23.png')
         col1,col2,col3,col4=st.columns(4)
         
         with col4:
                 def guardarimagen():
-                      plt.savefig('graficoviejo.png')  
+                      plt.savefig('https://github.com/elalerivas/Post-tuloUTN/graficoviejo.png')  
                 with open('.\grafico.png', 'rb') as archivo:
                         descargar=st.download_button('Descargar gráfico', archivo, file_name='Grafico_SP2D.png',
                         mime='image/png')
